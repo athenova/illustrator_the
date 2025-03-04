294c33b9f726ff39a64bebefa2684380c4dd4842
athenova <aleksey.sergeyev@yandex.com>
2025-03-04 23:08:11 +0300

    Перевод на simple_blogger

---
+++ project.py
@@ -0,0 +1,41 @@
+from simple_blogger import CommonBlogger
+from datetime import datetime
+
+class Project(CommonBlogger):
+    def _example_task_creator(self):
+        return [
+            {
+                "author": "Author",
+                "book": "Book",
+                "name": "Character",
+            }
+        ]
+
+    def _get_category_folder(self, task):
+        return f"{task['author']}/{task['book']}"
+                    
+    def _get_topic_folder(self, task):
+        return f"{task['entity']}"
+
+    def _system_prompt(self, task):
+        return "Ты - книгоман"
+
+    def _task_converter(self, idea):
+        return { 
+                    "author": idea['author'],
+                    "book": idea['book'],
+                    "entity": idea['name'],
+                    "topic_prompt": f"Опиши '{idea['name']}'({idea['description'] if 'description' in idea else ''}) из книги '{idea['book']}' автора {idea['author']}, используй не более {self.topic_word_limit} слов, используй смайлики",
+                    "topic_image": f"Нарисуй рисунок, вдохновлённый '{idea['name']}'({idea['description'] if 'description' in idea else ''}) из книги '{idea['book']}' автора {idea['author']}",
+                }
+
+    def __init__(self, **kwargs):
+        super().__init__(
+            review_chat_id=-1002374309134,
+            first_post_date=datetime(2025, 3, 5),
+            text_ai_token_name='OPENAI_API_KEY',
+            ai_text_model='chatgpt-4o-latest',
+            text_base_url='https://api.openai.com/v1',
+            topic_word_limit=100,
+            **kwargs
+        )
--- send.py
+++ send.py
@@ -1,25 +1,5 @@
-import os
-import telebot
-import json
-import glob
+from project import Project
 
-from datetime import date
-
-BOT_TOKEN_NAME = "ATHE_BOT_TOKEN"
-BOT_TOKEN = os.environ.get(BOT_TOKEN_NAME)
-CHAT_ID = '@illustrator_the'
-
-check_date = date.today()
-
-tasks = json.load(open('files/in_progress/tasks.json', 'rt', encoding='UTF-8'))
-
-for i, task in enumerate(tasks):
-    if task["date"] == check_date.strftime('%Y-%m-%d'):
-        folder_name = glob.escape(f"files/in_progress/{task['folder'].replace('/', ',')}/data/{task['name'].replace('/', ',')}")
-        text_file_name = f"{folder_name}/text.txt"
-        image_file_name = f"{folder_name}/image.png"
-
-        bot = telebot.TeleBot(BOT_TOKEN)
-        if os.path.exists(text_file_name) and os.path.exists(image_file_name):
-            bot.send_photo(chat_id=CHAT_ID, photo=open(image_file_name, 'rb'), disable_notification=True)
-            bot.send_message(chat_id=CHAT_ID, text=open(text_file_name, 'rt', encoding='UTF-8').read(), parse_mode="Markdown")
+if __name__ == '__main__':
+    project = Project()
+    project.send()
